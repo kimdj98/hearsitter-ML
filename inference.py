251ccd2cd2c222b09b6405a18f9f1726d33b5b6b
kimdj98 <kimdj98g@gmail.com>
2023-03-19 01:02:45 +0900

    add bicycle bell sound

--- inference.py
+++ inference.py
@@ -17,9 +17,8 @@ import pdb
 
 parser = argparse.ArgumentParser(description='Example of parser. ')
 # model name decides, which pre-trained model is loaded
-parser.add_argument('--model_name', type=str, default='mn40_as_ext')
 parser.add_argument('--cuda', action='store_true', default=False)
-parser.add_argument('--audio_path', type=str, required=False, default="resources/9rw3qz2ouRQ_290.wav")
+parser.add_argument('--audio_path', type=str, required=True)
 
 # preprocessing
 parser.add_argument('--sample_rate', type=int, default=32000)
@@ -27,11 +26,17 @@ parser.add_argument('--window_size', type=int, default=800)
 parser.add_argument('--hop_size', type=int, default=320)
 parser.add_argument('--n_mels', type=int, default=128)
 
+# ---------------------------------------- Choose model ----------------------------------------
+parser.add_argument('--model_name', type=str, default='mn40_as_ext')
+
 # overwrite 'model_name' by 'ensemble_model' to evaluate an ensemble
+# We don't use model from model_name if ensemble argument is not empty
+
 # parser.add_argument('--ensemble', nargs='+', default=["mn40_as_ext"])
 # parser.add_argument('--ensemble', nargs='+', default=["mn40_as"])
 # parser.add_argument('--ensemble', nargs='+', default=["mn40_as_no_im_pre"])
 parser.add_argument('--ensemble', nargs='+', default=["mn40_as", "mn40_as_ext"])
+# ----------------------------------------------------------------------------------------------
 
 """
 Running Inference on an audio clip.
@@ -109,7 +114,10 @@ def inference(audio_path):
     result.update({"Machine Gun": preds[428]}) # 0.4
     result.update({"Fusillade": preds[429]}) # 0.1
     output.update({"Gunshot": preds[427]*0.5 + preds[428]*0.4 + preds[429]*0.1})
-
+    # Bicycle 
+    result.update({"Bicycle": preds[341]}) # 0.1
+    result.update({"Bicycle Bell": preds[203]}) # 0.9
+    output.update({"Bicycle Bell": preds[341]*0.1 + preds[203]*0.9})
 
     # Print audio tagging top probabilities
     # print("************* Acoustic Event Detected: *****************")
@@ -167,110 +175,3 @@ def audio_tagging(args):
         print('{}: {:.3f}'.format(labels[sorted_indexes[k]],
             preds[sorted_indexes[k]]))
     print("********************************************************")
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='Example of parser. ')
-#     # model name decides, which pre-trained model is loaded
-#     parser.add_argument('--model_name', type=str, default='mn10_as')
-#     parser.add_argument('--cuda', action='store_true', default=False)
-#     parser.add_argument('--audio_path', type=str, required=True)
-
-#     # preprocessing
-#     parser.add_argument('--sample_rate', type=int, default=32000)
-#     parser.add_argument('--window_size', type=int, default=800)
-#     parser.add_argument('--hop_size', type=int, default=320)
-#     parser.add_argument('--n_mels', type=int, default=128)
-
-#     # overwrite 'model_name' by 'ensemble_model' to evaluate an ensemble
-#     parser.add_argument('--ensemble', nargs='+', default=[])
-
-#     args = parser.parse_args()
-
-#     """
-#     Running Inference on an audio clip.
-#     """
-#     model_name = args.model_name
-#     device = torch.device('cuda') if args.cuda and torch.cuda.is_available() else torch.device('cpu')
-#     audio_path = args.audio_path
-#     sample_rate = args.sample_rate
-#     window_size = args.window_size
-#     hop_size = args.hop_size
-#     n_mels = args.n_mels
-
-#     # load pre-trained model
-#     if len(args.ensemble) > 0:
-#         model = get_ensemble_model(args.ensemble)
-#     else:
-#         model = get_mobilenet(width_mult=NAME_TO_WIDTH(model_name), pretrained_name=model_name)
-#     model.to(device)
-#     model.eval()
-    
-#     # model to preprocess waveform into mel spectrograms
-#     mel = AugmentMelSTFT(n_mels=n_mels, sr=sample_rate, win_length=window_size, hopsize=hop_size)
-#     mel.to(device)
-#     mel.eval()
-
-#     audio_paths = glob.glob("resources/*.wav")
-#     print(audio_paths)
-
-#     df = pd.DataFrame(labels)
-
-#     print(df[0][300])
-
-#     #############################################################
-#     # test inference one time
-#     #############################################################
-#     print("\nCheck If model is loaded\n")
-#     audio_path = "resources/-3Y4NuyZmvA_250.wav"
-
-#     (waveform, _) = librosa.core.load(audio_path, sr=sample_rate, mono=True)
-#     waveform = torch.from_numpy(waveform[None, :]).to(device)
-
-#     # our models are trained in half precision mode (torch.float16)
-#     # run on cuda with torch.float16 to get the best performance
-#     # running on cpu with torch.float32 gives similar performance, using torch.bfloat16 is worse
-#     with torch.no_grad(), autocast(device_type=device.type) if args.cuda else nullcontext():
-#         spec = mel(waveform)
-#         preds, features = model(spec.unsqueeze(0))
-#     preds = torch.sigmoid(preds.float()).squeeze().cpu().numpy()
-
-#     sorted_indexes = np.argsort(preds)[::-1]
-
-#     # Print audio tagging top probabilities
-#     print("************* Acoustic Event Detected: *****************")
-#     for k in range(10):
-#         print('{}: {:.3f}'.format(labels[sorted_indexes[k]],
-#             preds[sorted_indexes[k]]))
-#     print("********************************************************")
-#     print("\nModel loaded successfully\n")
-
-#     #############################################################
-#     # test inference for one time
-#     #############################################################
-#     print(inference(audio_path))
-
-    
-#     # # testing if infrerence working
-#     # for audio_path in audio_paths:
-#     #     (waveform, _) = librosa.core.load(audio_path, sr=sample_rate, mono=True)
-#     #     waveform = torch.from_numpy(waveform[None, :]).to(device)
-
-#     #     # our models are trained in half precision mode (torch.float16)
-#     #     # run on cuda with torch.float16 to get the best performance
-#     #     # running on cpu with torch.float32 gives similar performance, using torch.bfloat16 is worse
-#     #     with torch.no_grad(), autocast(device_type=device.type) if args.cuda else nullcontext():
-#     #         spec = mel(waveform)
-#     #         preds, features = model(spec.unsqueeze(0))
-#     #     preds = torch.sigmoid(preds.float()).squeeze().cpu().numpy()
-
-#     #     sorted_indexes = np.argsort(preds)[::-1]
-
-#     #     # Print audio tagging top probabilities
-#     #     print("************* Acoustic Event Detected: *****************")
-#     #     for k in range(10):
-#     #         print('{}: {:.3f}'.format(labels[sorted_indexes[k]],
-#     #             preds[sorted_indexes[k]]))
-#     #     print("********************************************************")
-
-#     # audio_tagging(args)
-    
